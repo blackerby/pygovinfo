43fc373a52b79b9fc56d9f8cb03640f2b467c357
William Blackerby <wmblackerby@gmail.com>
2025-06-19 21:22:25 -0400

    modernize the newtype definition

--- src/govinfo/config.py
+++ src/govinfo/config.py
@@ -1,7 +1,5 @@
-from typing import NewType
-
 BASE_URL = "https://api.govinfo.gov"
 PAGE_DEFAULT = 20
 OFFSET_DEFAULT = "*"
 
-RequestArgs = NewType("RequestArgs", tuple[str, dict])
+RequestArgs = tuple[str, dict]
