42a458aba4471887a0f917c4e79e34223d1bb082
William Blackerby <wmblackerby@gmail.com>
2025-06-20 16:06:39 -0400

    experiment with not using pydantic

--- src/govinfo/collections.py
+++ src/govinfo/collections.py
@@ -1,26 +1,5 @@
 from govinfo.config import RequestArgs
 from govinfo.exceptions import GovinfoException
-from govinfo.models import GovinfoModel
-from govinfo.packages import PackageInfo
-
-
-class SummaryItem(GovinfoModel):
-    collection_code: str
-    collection_name: str
-    package_count: int
-    granule_count: int | None
-
-
-class CollectionSummary(GovinfoModel):
-    collections: list[SummaryItem]
-
-
-class CollectionContainer(GovinfoModel):
-    count: int
-    message: str | None
-    next_page: str | None
-    previous_page: str | None
-    packages: list[PackageInfo]
 
 
 class CollectionsMixin:
@@ -53,9 +32,4 @@ class CollectionsMixin:
         except GovinfoException as e:
             raise e
 
-        if collection is None:
-            validated = CollectionSummary(**result.data)
-        else:
-            validated = CollectionContainer(**result.data)
-        # TODO: dump with(out) alias?
-        return validated.model_dump()
+        return result.data
