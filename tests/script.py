0d1c0172aef98459863c4decea6bd7e4b10041d2
William Blackerby <wmblackerby@gmail.com>
2025-06-24 11:07:53 -0400

    moving stuff around

--- tests/script.py
+++ tests/script.py
@@ -6,14 +6,14 @@ from govinfo import GovInfo
 api_key = os.getenv("GPO_API_KEY")
 govinfo = GovInfo(api_key=api_key)
 start_date = "2025-06-20T00:00:00Z"
-collections = govinfo.collections()
-bills = govinfo.collection("bills", start_date=start_date)
 package_id = "CREC-2018-01-04"
 granule_id = "CREC-2018-01-04-pt1-PgD7-2"
 
+collections = govinfo.collections()
 for collection in collections:
     pprint(collection)
 
+bills = govinfo.collection("bills", start_date=start_date)
 for package in bills:
     pprint(package)
 
